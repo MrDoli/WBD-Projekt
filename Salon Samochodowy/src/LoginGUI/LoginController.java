a3b719ec1e58c245ce4eaba2b74728a3fdcd5ef3
MrDoli <doli33@op.pl>
2019-01-08 00:15:42 +0100

    Po zrobieniu wstepnego głównego okna do aplikacji

--- Salon Samochodowy/src/LoginGUI/LoginController.java
+++ Salon Samochodowy/src/LoginGUI/LoginController.java
@@ -1,4 +1,29 @@
 package LoginGUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 public class LoginController {
+
+    @FXML
+    private Button administrator = new Button();
+    @FXML
+    private Button wlascicielSalonu = new Button();
+    @FXML
+    private Button kierownik = new Button();
+    @FXML
+    private Button ksiegowa = new Button();
+    @FXML
+    private Button sprzedawca = new Button();
+    @FXML
+    private Button klient = new Button();
+    @FXML
+    private VBox vBox = new VBox();
+
+    public LoginController(){}
+
+    @FXML
+    private void initialize() {}
+
 }
